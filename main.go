1a95b4a70791e5ff4c2c28335d9781b49810cb1e
Pixels67 <pixels67@proton.me>
2026-03-16 20:32:03 +0300

    Make app compatible with Render

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -19,8 +20,13 @@ func main() {
 
 	router := gin.Default()
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	router.GET("/posts/:file", retrieve)
-	router.Run("localhost:8080")
+	router.Run("0.0.0.0:" + port)
 }
 
 func retrieve(c *gin.Context) {
